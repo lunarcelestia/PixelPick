61646d215e98cbca84dcb2502050c970b4f3bd22
lunar celestia <mierrored@gmail.com>
2025-04-01 00:19:23 +0300

    Update KeepAliveService.cs

--- KeepAliveService.cs
+++ KeepAliveService.cs
@@ -1,16 +1,46 @@
-﻿using System;
-using System.Threading;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace PixelPick
+public class KeepAliveService
 {
-    public class KeepAliveService
-    {
+    private readonly string _botUrl;
+    private readonly HttpClient _httpClient;
+    private readonly CancellationTokenSource _cancellationTokenSource;
 
+    public KeepAliveService(string botUrl)
+    {
+        _botUrl = botUrl;
+        _httpClient = new HttpClient();
+        _cancellationTokenSource = new CancellationTokenSource();
+    }
+
+    public void Start()
+    {
+        Task.Run(async () => await KeepAliveLoop(_cancellationTokenSource.Token));
+    }
+
+    public void Stop()
+    {
+        _cancellationTokenSource.Cancel();
+    }
+
+    private async Task KeepAliveLoop(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync(_botUrl);
+                Console.WriteLine($"Keep-alive pinged {_botUrl}. Status code: {response.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Keep-alive failed: {ex.Message}");
+            }
+
+            await Task.Delay(TimeSpan.FromMinutes(5), cancellationToken);
+        }
     }
 }
