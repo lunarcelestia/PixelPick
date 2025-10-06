0809916ead726af373706cd81962e30c15668d83
lunar celestia <mierrored@gmail.com>
2025-10-06 17:25:54 +0300

    add subscription

---
+++ User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelPick
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public long ChatId { get; set; }
+        public List<Subscription> Subscriptions { get; set; }
+        public List<string> SearchHistory { get; set; }
+        public List<string> Interests { get; set; }
+        public bool IsSubscribed { get; set; }
+    }
+
+    public class SteamGame
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public string GameName { get; set; }
+    }
+
+}
---
+++ UserSubscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelPick
+{
+    public class UserSubscription
+    {
+        public long UserId { get; set; }
+        public bool IsSubscribed { get; set; }
+        public List<string> Interests { get; set; } = new List<string>();
+    }
+}
